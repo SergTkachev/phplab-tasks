d06555a4e314927ff6faf0d7b4b45867da6423c5
SergTkachev <77504957+SergTkachev@users.noreply.github.com>
2021-02-08 20:15:07 +0200

    Add files via upload

--- src/01-basics.php
+++ src/01-basics.php
@@ -3,7 +3,7 @@
  * The $minute variable contains a number from 0 to 59 (i.e. 10 or 25 or 60 etc).
  * Determine in which quarter of an hour the number falls.
  * Return one of the values: "first", "second", "third" and "fourth".
- * Throw InvalidArgumentException if $minute is negative of greater than 60.
+ * Throw InvalidArgumentException if $minute is negative of greater then 60.
  * @see https://www.php.net/manual/en/class.invalidargumentexception.php
  *
  * @param  int  $minute
@@ -11,36 +11,50 @@
  * @throws InvalidArgumentException
  */
 
-function getMinuteQuarter(int $minute)
+function getMinuteQuarter($minute)
 {
-if ($minute >= 0 && $minute <= 14) {
-	echo('first');
+
+
+    if ($minute >= 1 and $minute <= 15) {
+        return rtrim("first");
+    }
+
+    if ($minute >= 16 and $minute <= 30) {
+        return rtrim("second");
+    }
+
+    if ($minute >= 31 and $minute <= 45) {
+        return rtrim("third");
+    }
+
+
+    if ($minute >= 46 and $minute <= 59 ) {
+        return rtrim("fourth");
+    }
+
+
+    if($minute > 61){
+        throw new InvalidArgumentException("must be < than 60");
+    }
+
+    if ($minute <= 0) {
+        return rtrim("fourth");
+    }
 }
-if ($minute >= 15 && $minute <= 30) {
-	echo('second');
+
+try{
+    echo getMinuteQuarter(75);
+
+}catch (\InvalidArgumentException $e){
+    echo $e->getMessage();
 }
-if ($minute >= 31 && $minute <= 45) {
-	echo('third');
-}
-if ($minute >= 46 && $minute <= 60) {
-	echo('fourth');  
-    
-}
-if($min<0 && $min>=61)
-	{
-		return InvalidArgumentException;
-		echo 'Ohibka';
-	}
-}
-getMinuteQuarter(10)
-//$min = 10;
 
 
 
 /**
  * The $year variable contains a year (i.e. 1995 or 2020 etc).
  * Return true if the year is Leap or false otherwise.
- * Throw InvalidArgumentException if $year is lower than 1900.
+ * Throw InvalidArgumentException if $year is lower then 1900.
  * @see https://en.wikipedia.org/wiki/Leap_year
  * @see https://www.php.net/manual/en/class.invalidargumentexception.php
  *
@@ -68,33 +82,49 @@ isLeapYear(2020)
  * The $input variable contains a string of six digits (like '123456' or '385934').
  * Return true if the sum of the first three digits is equal with the sum of last three ones
  * (i.e. in first case 1+2+3 not equal with 4+5+6 - need to return false).
- * Throw InvalidArgumentException if $input contains more or less than 6 digits.
+ * Throw InvalidArgumentException if $input contains more then 6 digits.
  * @see https://www.php.net/manual/en/class.invalidargumentexception.php
  *
  * @param  string  $input
  * @return boolean
  * @throws InvalidArgumentException
  */
-$str='123456';
-$str1='385934'; 
+function isSumEqual(string $input) {
 
-$str=(int)$str;
-$str1=(int)$str1;
+    if(mb_strlen($input) > 6 || mb_strlen($input) < 6 ) {
+        throw new InvalidArgumentException("should be 6 values");
 
-function isSumEqual(string $str){
-    $count= 0;
-    for($i=1;$i<6;$i++){
-        $str = (string)$i;
-        if(strlen($str)<6){
-            for($j=strlen($str);$j<6;$j++){
-                $str='0'.$str;
-            }
-        }
-        If($str[0]+$str[1]+$str[2] == $str[3]+$str[4]+$str[5]){  
-            $count++; echo 'Билет номер: '.$str.'<br>';
-        }
     }
-//echo 'Количество счастливых билетов: '.$count;
+
+    $arr1 = str_split($input);
+
+    $val1 = 0;
+    $val2 = 0;
+    $i = 0;
+
+    foreach($arr1 as $key => $value) {
+        if ($i < 3) {
+            $val1 += $value;
+        }
+        else {
+            $val2 += $value;
+        }
+
+        $i++;
+    }
+
+
+    if($val1 == $val2) {
+        return true;
+    } else {
+        return false;
+    }
 }
-echo isSumEqual($str1);
+
+try{
+    var_dump (isSumEqual("123456"));
+
+}catch (\InvalidArgumentException $e){
+    echo $e->getMessage();
+}
 
