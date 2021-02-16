aca6a149e4cac51f3bc1cf0eb1fd2c756e13a21c
SergTkachev <77504957+SergTkachev@users.noreply.github.com>
2021-02-16 22:01:44 +0200

    Add files via upload
    create DB

--- src/db/migrations.php
+++ src/db/migrations.php
@@ -28,12 +28,38 @@ require_once './pdo_ini.php';
 $sql = <<<'SQL'
 CREATE TABLE `cities` (
 	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(50) NOT NULL COLLATE 'utf8_general_ci',
+	`name` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
 	PRIMARY KEY (`id`)
 );
+
 SQL;
 $pdo->exec($sql);
 
 // TODO states
+$sql = <<<'SQL'
+CREATE TABLE `states` (
+	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(100) NOT NULL COLLATE 'utf8_general_ci',
+	PRIMARY KEY (`id`)
+);
+
+SQL;
+$pdo->exec($sql);
+
+
 
 // TODO airports
+$sql = <<<'SQL'
+CREATE TABLE `airports` (
+	`id` int(11) NOT NULL,
+	`name` varchar(255) NOT NULL,
+	`code` varchar(255) NOT NULL,
+	`city_id` int(100) NOT NULL,
+	`state_id` int(100) NOT NULL,
+	`address` varchar(255) NOT NULL,
+	`timezone` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`) 
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+  SQL;
+  $pdo->exec($sql);
